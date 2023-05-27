9beb140d16b5f8a58330e8dc9c02325f61a15ad2
Hồ Việt Hoàng <hoanghv.21it@vku.udn.vn>
2023-05-27 19:15:50 +0700

    update

---
+++ B1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace B1;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhập một số nguyên không âm có tối đa 3 chữ số: ");
+        int number = int.Parse(Console.ReadLine());
+        string words = "";
+
+        if (number < 0 || number > 999)
+        {
+            Console.WriteLine("Số không hợp lệ.");
+            return;
+        }
+
+        string[] units = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                           "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        string[] tens = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+        if (number == 0)
+        {
+            Console.WriteLine("zero");
+            return;
+        }
+
+        if ((number / 100) > 0)
+        {
+            words += units[number / 100] + " hundred ";
+            number %= 100;
+        }
+
+        if (number > 0)
+        {
+            if (words != "")
+                words += "and ";
+
+            if (number < 20)
+            {
+                words += units[number];
+            }
+            else
+            {
+                words += tens[number / 10];
+                if ((number % 10) > 0)
+                    words += " " + units[number % 10];
+            }
+        }
+
+        Console.WriteLine(words);
+    }
+}
+
---
+++ B2/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace B2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int option;
+            do 
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Draw the triangle");
+                Console.WriteLine("2. Draw the square");
+                Console.WriteLine("3. Draw the rectangle");
+                Console.WriteLine("0. Exit");
+      
+                Console.Write("Enter your option: ");
+                option = Convert.ToInt16(Console.ReadLine());
+
+                switch (option) 
+                {
+                    case 1:
+                        DrawTriangle();
+                        break;
+                    case 2:
+                        DrawSquare();
+                        break;
+                    case 3:
+                        DrawRectangle();
+                        break;
+                    default:
+                        Console.WriteLine("No choice!");
+                        break;
+                }
+            } while (option != 0);
+        }
+
+        static void DrawTriangle()
+        {
+            for (int i = 0; i < 5; i++) 
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    if (j >= 5 - i && j <= 5 + i)
+                    {
+                        Console.Write(" * ");
+                        continue;
+                    }
+                    Console.Write("   ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void DrawSquare() 
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine("   ");
+                
+            }
+        }
+
+        static void DrawRectangle() 
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine("   ");
+            }
+        }
+}
